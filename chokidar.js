5f0c37824e68263ebc4d8c7584d1515bebaf755a
hanif <hanif_1362@yahoo.com>
2025-10-19 20:47:49 +0200

    Add project skeleton and pipeline

---
+++ chokidar.js
@@ -0,0 +1,19 @@
+const chokidar = require('chokidar')
+const { spawn } = require('child_process')
+
+function run(command) {
+    const [cmd, ...args] = command.split(' ')
+    spawn(cmd, args, { stdio: 'inherit', shell: true })
+}
+
+chokidar.watch('src/js').on('all', () => {
+    run('npm run copy-js-assets')
+})
+
+chokidar.watch('assets').on('all', () => {
+    run('npm run pug')
+})
+
+chokidar.watch('src/pug').on('all', () => {
+    run('npm run scss')
+})
